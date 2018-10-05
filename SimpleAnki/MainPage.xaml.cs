aea59ea00a64903f9197d3697017b3a15c39f2b5
alexcys <choyuansu@gmail.com>
2018-10-05 00:57:12 -0700

    Initial commit. Working app and can run on local device.

---
+++ SimpleAnki/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;   
+using Xamarin.Forms;
+
+namespace SimpleAnki
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            label.Text = "0";
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            // Delegate command is another way
+            count++;
+            label.Text = count.ToString();
+        }
+    }
+}
